310f6e10dbecc3e8d54b5d3d3820bec5c05c1c8e
Jair Trejo <jair@jairtrejo.mx>
2012-08-16 22:42:15 -0500

    Initial commit.

---
+++ fabfile.py
@@ -0,0 +1,40 @@
+from fabric.api import local, hosts
+import os
+import fabric.contrib.project as project
+
+PROD = 'jtrejo@jairtrejo.mx'
+DEST_PATH = '/home/jtrejo/webapps/jtrejomx'
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
+
+
+def clean():
+    local('rm -rf ./deploy')
+
+
+def generate():
+    local('hyde -g -s .')
+
+
+def regen():
+    clean()
+    generate()
+
+
+def serve():
+    local('hyde -w -s . -k')
+
+
+def reserve():
+    regen()
+    serve()
+
+
+@hosts(PROD)
+def publish():
+    regen()
+    project.rsync_project(
+        remote_dir=DEST_PATH,
+        local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        delete=True
+    )
